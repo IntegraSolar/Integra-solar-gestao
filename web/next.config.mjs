48265a3d788e337f2fcd2abbda887d67979b240a
Hyperfocos <hyperfocos@gmail.com>
2026-06-22 02:53:57 -0300

    security: add Sentry error monitoring for production

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from '@sentry/nextjs'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -22,4 +24,7 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  silent: true,
+  hideSourceMaps: true,
+})
